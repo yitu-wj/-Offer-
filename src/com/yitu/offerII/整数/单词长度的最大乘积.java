84b2f0a056c22f4f8bd64a23617600ab1d25894f
Wang Jie <2551434573@qq.com>
2021-11-15 10:23:17 +0800

    单词长度的最大乘积

---
+++ src/com/yitu/offerII/整数/单词长度的最大乘积.java
@@ -0,0 +1,50 @@
+package com.yitu.offerII.整数;
+
+/**
+ * https://leetcode-cn.com/problems/aseY1I/
+ */
+public class 单词长度的最大乘积 {
+    public int maxProduct1(String[] words) {
+        boolean[][] flags = new boolean[words.length][26];
+        for (int i = 0; i < words.length; i++) {
+            for (char c : words[i].toCharArray()) {
+                flags[i][c - 'a'] = true;
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                int k = 0;
+                for (; k < 26; k++) {
+                    if (flags[i][k] && flags[j][k]) {
+                        break;
+                    }
+                }
+                if (k == 26) {
+                    int prod = words[i].length() * words[j].length();
+                    result = Math.max(result, prod);
+                }
+            }
+        }
+        return result;
+    }
+
+    public int maxProduct2(String[] words) {
+        int[] flags = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            for (char ch : words[i].toCharArray()) {
+                flags[i] |= 1 << (ch - 'a');
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if ((flags[i] & flags[j]) == 0) {
+                    int prod = words[i].length() * words[j].length();
+                    result = Math.max(result, prod);
+                }
+            }
+        }
+        return result;
+    }
+}
