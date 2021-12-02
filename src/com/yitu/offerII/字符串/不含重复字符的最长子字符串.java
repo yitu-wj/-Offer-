8038a24241cc48c1eecca44acaa791074ea237c4
Wang Jie <2551434573@qq.com>
2021-12-02 13:17:00 +0800

    不含重复字符的最长子字符串

--- src/com/yitu/offerII/字符串/不含重复字符的最长子字符串.java
+++ src/com/yitu/offerII/字符串/不含重复字符的最长子字符串.java
@@ -8,6 +8,33 @@ public class 不含重复字符的最长子字符串 {
         if (s.length() == 0) {
             return 0;
         }
+
+        int[] counts = new int[256];
+        int i = 0;
+        int j = -1;
+        int longest = 1;
+        int countDup = 0;
+        for (; i < s.length(); i++) {
+            counts[s.charAt(i)]++;
+            if (counts[s.charAt(i)] == 2) {
+                countDup++;
+            }
+            while (countDup > 0) {
+                j++;
+                counts[s.charAt(j)]--;
+                if (counts[s.charAt(j)] == 1) {
+                    countDup--;
+                }
+            }
+            longest = Math.max(i - j, longest);
+        }
+        return longest;
+    }
+
+    public int lengthOfLongestSubstring1(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
         int[] counts = new int[256];
         int i = 0;
         int j = -1;
