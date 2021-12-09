dd4a7100aca0c500ab081515b5ef4be3637f6796
Wang Jie <2551434573@qq.com>
2021-12-09 09:12:52 +0800

    展平多级双向链表

---
+++ src/com/yitu/offerII/链表/展平多级双向链表.java
@@ -0,0 +1,38 @@
+package com.yitu.offerII.链表;
+
+import com.yitu.offerII.Node;
+
+/**
+ * https://leetcode-cn.com/problems/Qv1Da2/
+ */
+public class 展平多级双向链表 {
+    public Node flatten(Node head) {
+        flattenGatTail(head);
+        return head;
+    }
+
+    private Node flattenGatTail(Node head) {
+        Node node = head;
+        Node tail = null;
+        while (node != null) {
+            Node next = node.next;
+            if (node.child != null) {
+                Node child = node.child;
+                Node childTail = flattenGatTail(node.child);
+
+                node.child = null;
+                node.next = child;
+                child.prev = node;
+                childTail.next = next;
+                if (next != null) {
+                    next.prev = childTail;
+                }
+                tail = childTail;
+            } else {
+                tail = node;
+            }
+            node = next;
+        }
+        return tail;
+    }
+}
