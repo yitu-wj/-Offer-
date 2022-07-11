e35786cc7d24975caa8268d38fed5517714cba43
Wang Jie <2551434573@qq.com>
2022-07-11 20:56:44 +0800

    数组相对排序

---
+++ src/com/yitu/offerII/排序/数组相对排序.java
@@ -0,0 +1,29 @@
+package com.yitu.offerII.排序;
+
+/**
+ * https://leetcode.cn/problems/0H97ZC/
+ */
+public class 数组相对排序 {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] counts = new int[1001];
+        for (int num : arr1) {
+            counts[num]++;
+        }
+
+        int i = 0;
+        for (int num : arr2) {
+            while (counts[num] > 0) {
+                arr1[i++] = num;
+                counts[num]--;
+            }
+        }
+
+        for (int num = 0; num < counts.length; num++) {
+            while (counts[num] > 0) {
+                arr1[i++] = num;
+                counts[num]--;
+            }
+        }
+        return arr1;
+    }
+}
