5af631d86f618b2754e3f906bc055de27f10679a
Wang Jie <2551434573@qq.com>
2022-07-18 20:29:04 +0800

    爬楼梯的最少成本

---
+++ src/com/yitu/offerII/动态规划/爬楼梯的最少成本.java
@@ -0,0 +1,30 @@
+package com.yitu.offerII.动态规划;
+
+
+/**
+ * https://leetcode.cn/problems/GzCJIP/
+ */
+public class 爬楼梯的最少成本 {
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        int[] dp = new int[len];
+        helper(cost, len - 1, dp);
+        return Math.min(dp[len - 2], dp[len - 1]);
+    }
+
+    private void helper(int[] cost, int i, int[] dp) {
+        if (i < 2) {
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+        } else if (dp[i] == 0) {
+            helper(cost, i - 2, dp);
+            helper(cost, i - 1, dp);
+            dp[i] = Math.min(dp[i - 2], dp[i - 1]) + cost[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        int i = new 爬楼梯的最少成本().minCostClimbingStairs(new int[]{1, 100});
+        System.out.println(i);
+    }
+}
